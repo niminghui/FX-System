dd3936cc48d86170cf1efa1e2d045f5f423fd46a
Steve.Ni <gent_ni@126.com>
2019-03-14 09:56:33 +0800

    add schema.sql and UuidTools.java

---
+++ fx-account/src/main/java/com/shiep/fxaccount/FxAccountApplication.java
@@ -0,0 +1,13 @@
+package com.shiep.fxaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxAccountApplication.class, args);
+    }
+
+}
