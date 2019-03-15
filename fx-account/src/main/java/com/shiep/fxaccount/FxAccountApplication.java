e86ce3430fca5aabd8879a557534902cbfa6cf90
Steve.Ni <gent_ni@126.com>
2019-03-15 10:25:38 +0800

    搭建服务注册中心--fx-account

--- fx-account/src/main/java/com/shiep/fxaccount/FxAccountApplication.java
+++ fx-account/src/main/java/com/shiep/fxaccount/FxAccountApplication.java
@@ -2,8 +2,14 @@ package com.shiep.fxaccount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+// 启用Feign
+@EnableFeignClients(basePackages = "com.shiep.fxaccount.endpoint")
+// 启用断路器
+@EnableCircuitBreaker
 public class FxAccountApplication {
 
     public static void main(String[] args) {
