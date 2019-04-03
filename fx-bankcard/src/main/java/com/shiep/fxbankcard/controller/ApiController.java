fe99cb17ec04daa4c84d870a5235c0555383f282
Steve.Ni <gent_ni@126.com>
2019-04-03 16:54:07 +0800

    fx-bankcard：新增ApiController

---
+++ fx-bankcard/src/main/java/com/shiep/fxbankcard/controller/ApiController.java
@@ -0,0 +1,45 @@
+package com.shiep.fxbankcard.controller;
+
+import com.shiep.fxbankcard.api.FxRateApi;
+import com.shiep.fxbankcard.api.HeadlinesApi;
+import com.shiep.fxbankcard.api.VerificationIDCardApi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author: 倪明辉
+ * @date: 2019/4/3 15:13
+ * @description: api控制层
+ */
+@RestController
+@RequestMapping(path = "/api", produces = "application/json;charset=utf-8")
+public class ApiController {
+    @GetMapping("/rmbRate")
+    public Map<String, Object> getRmbRate() {
+        return FxRateApi.getRmbRate();
+    }
+
+    @GetMapping("/fxRate")
+    public Map<String, Object> getFxRate() {
+        return FxRateApi.getFxRate();
+    }
+
+    @GetMapping("/headlines/{type}")
+    public Map<String, Object> getHeadlines(@PathVariable("type") String type) {
+        return HeadlinesApi.getHeadlines(type);
+    }
+
+    @GetMapping("/headlines")
+    public Map<String, Object> getHeadlines() {
+        return HeadlinesApi.getHeadlines(HeadlinesApi.TYPE_INTERNATIONAL);
+    }
+
+    @GetMapping("/verify/{idCardNum}")
+    public Map<String, Object> verify(@PathVariable("idCardNum") String idCardNum) {
+        return VerificationIDCardApi.verify(idCardNum);
+    }
+}
