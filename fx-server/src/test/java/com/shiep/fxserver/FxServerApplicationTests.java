ba46fbae9253c7e393c9e56ebd0f2094df707bb4
Steve.Ni <gent_ni@126.com>
2019-05-09 10:27:53 +0800

    FX-Server：搭建ELK分布式日志系统

--- fx-server/src/test/java/com/shiep/fxserver/FxServerApplicationTests.java
+++ fx-server/src/test/java/com/shiep/fxserver/FxServerApplicationTests.java
@@ -2,15 +2,20 @@ package com.shiep.fxserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FxServerApplicationTests {
+    private Logger logger = LoggerFactory.getLogger(FxServerApplicationTests.class);
 
     @Test
     public void contextLoads() {
+        logger.info("测试log");
+        logger.warn("warn log test");
     }
 
 }
